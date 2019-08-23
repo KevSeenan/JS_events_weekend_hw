6d5c46d26d5eec5bfd38d88096aa8d34b4d2deea
KevSeenan <kev.seenan@gmail.com>
2019-08-23 16:59:47 +0100

    Adds app.js document eventListene

--- js/app.js
+++ js/app.js
@@ -7,3 +7,37 @@ document.addEventListener('DOMContentLoaded', () => {
   const deleteAllButton = document.querySelector('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllClick);
 });
+
+const handleNewFormSubmit = function (event) {
+  event.preventDefault();
+
+  const bandListItem = createBandListItem(event.target);
+  const bandList = document.querySelector('#band-list');
+  bandList.appendChild(bandListItem);
+
+  event.target.reset();
+}
+
+const createBandListItem = function (form) {
+  const bandListItem = document.createElement('div');
+  bandListItem.classList.add('band-list-item');
+
+  const band = document.createElement('h2');
+  band.textContent = form.band.value;
+  bandListItem.appendChild(band);
+
+  const album = document.createElement('h3');
+  album.textContent = form.album.value;
+  bandListItem.appendChild(album);
+
+  const genre = document.createElement('p');
+  genre.textContent = form.genre.value;
+  bandListItem.appendChild(genre);
+
+  return bandListItem;
+}
+
+const handleDeleteAllClick = function (event) {
+  const bandList = document.querySelector('#band-list');
+  bandList.innerHTML = '';
+} 
