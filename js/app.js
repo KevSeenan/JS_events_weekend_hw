44a775ab539e1089c4ff9b08d596194d02c9caa3
KevSeenan <kev.seenan@gmail.com>
2019-08-23 16:41:23 +0100

    Adds app.js document eventListener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-document.addEvenListener('DOMContentoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-form-item');
+  form.addEventListener('submit', handleNewFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
 });
